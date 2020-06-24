ad536f0d766279771d5dabd55745f6b5b28ab6c7
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-24 01:59:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
-sentence = ("Hello there, and how are you?")
-
 def reverse_each_word(sentence)
+  words_array = sentence.split("")
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse}
+  new_array.join("")
 
 
   end
