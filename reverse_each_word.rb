e90786a949c4e506144434882366910431dc5e82
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-24 02:02:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
-  words_array = sentence.split("")
-  new_array = []
-  new_array = words_array.collect {|word| word.reverse}
-  new_array.join("")
-
-
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
   end
+  return_array.join(" ")
+end
